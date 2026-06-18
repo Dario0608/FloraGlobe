82a5e55a063d5bf3a140cc0fbd38530053ba2bd1
DarioRM <rochemarind@gmail.com>
2026-06-18 15:07:55 +0000

    Initialize project structre with base globe setup

---
+++ flora.js
@@ -0,0 +1,5 @@
+const world = Globe()
+
+(document.getElementById("globeViz"))
+.globeImageUrl("https://unpkg.com/three-globe/example/img/earth-blue-marble.jpg")
+.backgroundImageUrl("https://unpkg.com/three-globe/example/img/night-sky.png");
